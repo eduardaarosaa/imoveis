26ca928e73c2d7be22c24fe8cd1af05a1c9fcd80
Duda <eduardacirina@gmail.com>
2019-11-10 15:27:57 -0300

    Ok

--- resources/views/detalhes.blade.php
+++ resources/views/detalhes.blade.php
@@ -12,83 +12,89 @@
                         {{$detalhes->title}}</h5>
                     <div class="row">
                         <div class="col-md-12">
-                            @foreach($detalhes->getMedia() as $media)
 
+
+                            @foreach($detalhes->getMedia() as $media)
                             <img src="{{asset($media->getUrl('thumb'))}}" class="img-fluid" width="500">
+
                             @endforeach
                         </div>
 
-                    </div>
 
-                    <br>
-                    <br>
-                    <h5 class="card-title">
-                        R$ {{$detalhes->value_properties}}</h5>
-
-                    <h5>Descrição do Imóvel:</h5>
-                    <p class=" card-text">
-                        {{$detalhes->description}}
-                    </p>
-
-                    <i class="fas fa-map-marker-alt" size="3x"></i>
-                    <h5> Localização:</h5>
-
-                    <p class=" card-text">
-                        {{$detalhes->location}}
-                    </p>
-                    <div class="row">
-                        <div class="col-md-4">
-                            <h5>Qnt. Quartos</h5>
-                            <p class=" card-text">
-                                {{$detalhes->room}}
-                            </p>
-                        </div>
-                        <div class="col-md-4">
-                            <h5>Qnt. Banheiros</h5>
-                            <p class=" card-text">
-                                {{$detalhes->bathroom}}
-                            </p>
-                        </div>
-                        <div class="col-md-4">
-                            <h5>Mêtros</h5>
-                            <p class=" card-text">
-                                {{$detalhes->meters}}
-                            </p>
-                        </div>
 
                     </div>
-                    <div class="row">
-                        <div class="col-md-12">
-                            <h5>Características:</h5>
-                            <p class=" card-text">
-                                @foreach($detalhes->features as $row)
-                                <i class="fas fa-check"></i>
-                                {{$row->name}} <br>
-                                @endforeach
-                            </p>
-                        </div>
-                    </div>
+
                 </div>
-            </div>
-        </div>
-        <div class="col-md-4">
-            <div class="card" style="width: 18rem;">
-                <div class="card-body">
-                    <h5 class="card-title">Valor</h5>
-                    <a href="#" class="btn btn-success">R$ {{$detalhes->value_properties}}</a>
-                    <br><br>
 
-                    Quer saber mais detalhes?Chame agora!
-                    <a class="whats" href="https://wa.me/5511984127568">
-                        <img src="https://imagepng.org/wp-content/uploads/2017/08/WhatsApp-icone-3.png" width="30">
+                <br>
+                <br>
+                <h5 class="card-title">
+                    R$ {{$detalhes->value_properties}}</h5>
 
-                    </a>
+                <h5>Descrição do Imóvel:</h5>
+                <p class=" card-text">
+                    {{$detalhes->description}}
+                </p>
 
+                <i class="fas fa-map-marker-alt" size="3x"></i>
+                <h5> Localização:</h5>
 
+                <p class=" card-text">
+                    {{$detalhes->location}}
+                </p>
+                <div class="row">
+                    <div class="col-md-4">
+                        <h5>Qnt. Quartos</h5>
+                        <p class=" card-text">
+                            {{$detalhes->room}}
+                        </p>
+                    </div>
+                    <div class="col-md-4">
+                        <h5>Qnt. Banheiros</h5>
+                        <p class=" card-text">
+                            {{$detalhes->bathroom}}
+                        </p>
+                    </div>
+                    <div class="col-md-4">
+                        <h5>Mêtros</h5>
+                        <p class=" card-text">
+                            {{$detalhes->meters}}
+                        </p>
+                    </div>
+
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <h5>Características:</h5>
+                        <p class=" card-text">
+                            @foreach($detalhes->features as $row)
+                            <i class="fas fa-check"></i>
+                            {{$row->name}} <br>
+                            @endforeach
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
+    <div class="col-md-4">
+        <div class="card" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title">Valor</h5>
+                <a href="#" class="btn btn-success">R$ {{$detalhes->value_properties}}</a>
+                <br><br>
+
+                Quer saber mais detalhes?Chame agora!
+                <a class="whats" href="https://wa.me/5511984127568">
+                    <img src="https://imagepng.org/wp-content/uploads/2017/08/WhatsApp-icone-3.png" width="30">
+
+                </a>
+
+
+            </div>
+        </div>
+    </div>
+</div>
 </div>
 
 
